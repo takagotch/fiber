c8276d08efc0bd0eee6a86781417eec42b9b3786
takagotch <dyaccb@gmail.com>
2018-01-13 05:08:33 +0900

    puts f.transfer(1)

--- fiber.rb
+++ fiber.rb
@@ -100,7 +100,25 @@ g.rewind
 g = (1..10).to_enum
 loop { print g.next }
 
-#
+#transfer(1)
+require 'fiber'
 
+f = g = nil
+
+f = Fiber.new {|x|
+  puts "f1: #{x}"
+  x = g.transfer(x+1)
+  puts "f2: #{x}"
+  x = g.transfer(x+1)
+  puts "f3: #{x}"
+  x + 1
+}
+g = Fiber.new {|x|
+  puts "g1: #{x}"
+  x = f.transfer(x+1)
+  puts "g2: #{x}"
+  x = f.transfer(x+1)
+}
+puts f.transfer(1)
 
 
