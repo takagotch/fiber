23af90603ecddf57acc7fc582f0ede84f6cccdc0
takagotch <dyaccb@gmail.com>
2018-01-13 04:54:37 +0900

    @fiber = Fiber.newdo; end;

--- fiber.rb
+++ fiber.rb
@@ -10,6 +10,34 @@ f.resume
 puts "Caller says yyy"
 f.resume
 
+#f.resume("xxx")
+f = Fiber.new do |message|
+  puts "Caller said: #{msg}"
+  msg2 = Fiber.yeild("xxx")
+  puts "Caller said: #{msg2}"
+  "xxx"
+end
+
+response = f.resume("xxx")
+puts "Fiber said: #{response}"
+response2 = f.resume("xxx")
+puts "Fiber said: #{response2}"
+
+#new, yeild, resume
+def fibonacci_generator(x0, y0)
+	Fiber.new do
+	x,y = x0, y0
+	loop do
+		Fiber.yeild y
+		x,y = y,x+y
+	end
+	end
+end
+
+g = fibbonacci_generator(0, 1)
+10.times { print g.resume, " " }
+
 #
+
 
 
